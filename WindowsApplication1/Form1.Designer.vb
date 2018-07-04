ff9f24360443d516a4ce501552c1d6b15efb8e96
Maxim Sidorov <gen.maxsid@yandex.ru>
2018-07-04 22:44:35 +0300

    Проект игры "21 очко".

---
+++ WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,272 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Форма переопределяет dispose для очистки списка компонентов.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Является обязательной для конструктора форм Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'Примечание: следующая процедура является обязательной для конструктора форм Windows Forms
+    'Для ее изменения используйте конструктор форм Windows Form.  
+    'Не изменяйте ее в редакторе исходного кода.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.gk1 = New System.Windows.Forms.PictureBox()
+        Me.gk1_2 = New System.Windows.Forms.PictureBox()
+        Me.MyBall = New System.Windows.Forms.Label()
+        Me.PCBall = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.gk2 = New System.Windows.Forms.PictureBox()
+        Me.gk2_2 = New System.Windows.Forms.PictureBox()
+        Me.gk3 = New System.Windows.Forms.PictureBox()
+        Me.gk3_2 = New System.Windows.Forms.PictureBox()
+        Me.WinBall = New System.Windows.Forms.Label()
+        Me.LoseBall = New System.Windows.Forms.Label()
+        Me.DrowBall = New System.Windows.Forms.Label()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.Talk = New System.Windows.Forms.Label()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        CType(Me.gk1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.gk1_2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.gk2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.gk2_2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.gk3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.gk3_2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'gk1
+        '
+        Me.gk1.Location = New System.Drawing.Point(160, 234)
+        Me.gk1.Name = "gk1"
+        Me.gk1.Size = New System.Drawing.Size(65, 65)
+        Me.gk1.TabIndex = 0
+        Me.gk1.TabStop = False
+        '
+        'gk1_2
+        '
+        Me.gk1_2.Location = New System.Drawing.Point(275, 234)
+        Me.gk1_2.Name = "gk1_2"
+        Me.gk1_2.Size = New System.Drawing.Size(65, 65)
+        Me.gk1_2.TabIndex = 1
+        Me.gk1_2.TabStop = False
+        '
+        'MyBall
+        '
+        Me.MyBall.Font = New System.Drawing.Font("Microsoft Sans Serif", 44.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.MyBall.ForeColor = System.Drawing.Color.FromArgb(CType(CType(44, Byte), Integer), CType(CType(63, Byte), Integer), CType(CType(30, Byte), Integer))
+        Me.MyBall.Location = New System.Drawing.Point(33, 11)
+        Me.MyBall.Name = "MyBall"
+        Me.MyBall.Size = New System.Drawing.Size(104, 70)
+        Me.MyBall.TabIndex = 3
+        Me.MyBall.Text = "0"
+        Me.MyBall.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'PCBall
+        '
+        Me.PCBall.Font = New System.Drawing.Font("Microsoft Sans Serif", 44.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.PCBall.ForeColor = System.Drawing.Color.FromArgb(CType(CType(140, Byte), Integer), CType(CType(78, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.PCBall.Location = New System.Drawing.Point(365, 11)
+        Me.PCBall.Name = "PCBall"
+        Me.PCBall.Size = New System.Drawing.Size(104, 70)
+        Me.PCBall.TabIndex = 4
+        Me.PCBall.Text = "0"
+        Me.PCBall.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.Green
+        Me.PictureBox1.Image = Global.WindowsApplication1.My.Resources.Resources.кнопка1
+        Me.PictureBox1.Location = New System.Drawing.Point(217, 305)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(63, 52)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.PictureBox1.TabIndex = 6
+        Me.PictureBox1.TabStop = False
+        '
+        'gk2
+        '
+        Me.gk2.Location = New System.Drawing.Point(160, 168)
+        Me.gk2.Name = "gk2"
+        Me.gk2.Size = New System.Drawing.Size(65, 65)
+        Me.gk2.TabIndex = 9
+        Me.gk2.TabStop = False
+        '
+        'gk2_2
+        '
+        Me.gk2_2.Location = New System.Drawing.Point(275, 168)
+        Me.gk2_2.Name = "gk2_2"
+        Me.gk2_2.Size = New System.Drawing.Size(65, 65)
+        Me.gk2_2.TabIndex = 10
+        Me.gk2_2.TabStop = False
+        '
+        'gk3
+        '
+        Me.gk3.Location = New System.Drawing.Point(160, 102)
+        Me.gk3.Name = "gk3"
+        Me.gk3.Size = New System.Drawing.Size(65, 65)
+        Me.gk3.TabIndex = 11
+        Me.gk3.TabStop = False
+        '
+        'gk3_2
+        '
+        Me.gk3_2.Location = New System.Drawing.Point(275, 102)
+        Me.gk3_2.Name = "gk3_2"
+        Me.gk3_2.Size = New System.Drawing.Size(65, 65)
+        Me.gk3_2.TabIndex = 12
+        Me.gk3_2.TabStop = False
+        '
+        'WinBall
+        '
+        Me.WinBall.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.WinBall.ForeColor = System.Drawing.Color.Black
+        Me.WinBall.Location = New System.Drawing.Point(44, 222)
+        Me.WinBall.Name = "WinBall"
+        Me.WinBall.Size = New System.Drawing.Size(20, 25)
+        Me.WinBall.TabIndex = 13
+        Me.WinBall.Text = "0"
+        Me.WinBall.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+        '
+        'LoseBall
+        '
+        Me.LoseBall.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.LoseBall.ForeColor = System.Drawing.Color.Black
+        Me.LoseBall.Location = New System.Drawing.Point(102, 222)
+        Me.LoseBall.Name = "LoseBall"
+        Me.LoseBall.Size = New System.Drawing.Size(20, 25)
+        Me.LoseBall.TabIndex = 14
+        Me.LoseBall.Text = "0"
+        Me.LoseBall.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+        '
+        'DrowBall
+        '
+        Me.DrowBall.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.DrowBall.ForeColor = System.Drawing.Color.Black
+        Me.DrowBall.Location = New System.Drawing.Point(73, 222)
+        Me.DrowBall.Name = "DrowBall"
+        Me.DrowBall.Size = New System.Drawing.Size(20, 25)
+        Me.DrowBall.TabIndex = 15
+        Me.DrowBall.Text = "0"
+        Me.DrowBall.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.BackColor = System.Drawing.Color.Black
+        Me.PictureBox4.Image = Global.WindowsApplication1.My.Resources.Resources.oblako
+        Me.PictureBox4.Location = New System.Drawing.Point(240, 2)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(119, 62)
+        Me.PictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox4.TabIndex = 16
+        Me.PictureBox4.TabStop = False
+        Me.PictureBox4.Visible = False
+        '
+        'Talk
+        '
+        Me.Talk.BackColor = System.Drawing.Color.White
+        Me.Talk.Location = New System.Drawing.Point(257, 17)
+        Me.Talk.Name = "Talk"
+        Me.Talk.Size = New System.Drawing.Size(83, 28)
+        Me.Talk.TabIndex = 17
+        Me.Talk.Text = "У соперника больше очков"
+        Me.Talk.Visible = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.BackColor = System.Drawing.Color.Green
+        Me.PictureBox3.Enabled = False
+        Me.PictureBox3.Image = Global.WindowsApplication1.My.Resources.Resources.кнопка3
+        Me.PictureBox3.Location = New System.Drawing.Point(291, 305)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(63, 52)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.PictureBox3.TabIndex = 8
+        Me.PictureBox3.TabStop = False
+        Me.PictureBox3.Visible = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackColor = System.Drawing.Color.Green
+        Me.PictureBox2.Image = Global.WindowsApplication1.My.Resources.Resources.кнопка2
+        Me.PictureBox2.Location = New System.Drawing.Point(226, 305)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(63, 52)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.PictureBox2.TabIndex = 7
+        Me.PictureBox2.TabStop = False
+        Me.PictureBox2.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None
+        Me.BackgroundImage = Global.WindowsApplication1.My.Resources.Resources.старая
+        Me.ClientSize = New System.Drawing.Size(504, 400)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.Talk)
+        Me.Controls.Add(Me.PCBall)
+        Me.Controls.Add(Me.MyBall)
+        Me.Controls.Add(Me.DrowBall)
+        Me.Controls.Add(Me.LoseBall)
+        Me.Controls.Add(Me.WinBall)
+        Me.Controls.Add(Me.gk3_2)
+        Me.Controls.Add(Me.gk3)
+        Me.Controls.Add(Me.gk2_2)
+        Me.Controls.Add(Me.gk2)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.gk1_2)
+        Me.Controls.Add(Me.gk1)
+        Me.Controls.Add(Me.PictureBox4)
+        Me.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.MaximumSize = New System.Drawing.Size(520, 439)
+        Me.MinimumSize = New System.Drawing.Size(520, 439)
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.Text = " "
+        CType(Me.gk1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.gk1_2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.gk2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.gk2_2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.gk3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.gk3_2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents gk1 As System.Windows.Forms.PictureBox
+    Friend WithEvents gk1_2 As System.Windows.Forms.PictureBox
+    Friend WithEvents MyBall As System.Windows.Forms.Label
+    Friend WithEvents PCBall As System.Windows.Forms.Label
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents gk2 As System.Windows.Forms.PictureBox
+    Friend WithEvents gk2_2 As System.Windows.Forms.PictureBox
+    Friend WithEvents gk3 As System.Windows.Forms.PictureBox
+    Friend WithEvents gk3_2 As System.Windows.Forms.PictureBox
+    Friend WithEvents WinBall As System.Windows.Forms.Label
+    Friend WithEvents LoseBall As System.Windows.Forms.Label
+    Friend WithEvents DrowBall As System.Windows.Forms.Label
+    Friend WithEvents PictureBox4 As System.Windows.Forms.PictureBox
+    Friend WithEvents Talk As System.Windows.Forms.Label
+    Friend WithEvents PictureBox3 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox2 As System.Windows.Forms.PictureBox
+
+End Class
---
+++ WindowsApplication1/Form1.vb
@@ -0,0 +1,244 @@
+﻿Public Class Form1
+    Public xod, a, b As Integer
+    ' ****************************
+    '          Инфобар
+    '*****************************
+    Public Sub Pob()
+            If Val(MyBall.Text) > 21 And Val(PCBall.Text) <= 21 Then
+            Talk.Text = "У вас перебор!"
+                LoseBall.Text = Val(LoseBall.Text) + 1
+            Else
+                If Val(MyBall.Text) <= 21 And Val(PCBall.Text) > 21 Then
+                Talk.Text = "У соперника перебор!"
+                    WinBall.Text = Val(WinBall.Text) + 1
+            Else
+                If Val(MyBall.Text) > 21 And Val(PCBall.Text) > 21 Then
+                    Talk.Text = "Пербор у всех! Ничья."
+                    DrowBall.Text = Val(DrowBall.Text) + 1
+                Else
+                    If Val(MyBall.Text) < Val(PCBall.Text) Then
+                        Talk.Text = "У соперника больше очков!"
+                        LoseBall.Text = Val(LoseBall.Text) + 1
+                    Else
+                        If Val(MyBall.Text) > Val(PCBall.Text) Then
+                            Talk.Text = "У вас больше очков!"
+                            WinBall.Text = Val(WinBall.Text) + 1
+                        Else
+                            If Val(MyBall.Text) = Val(PCBall.Text) Then
+                                Talk.Text = "Ничья!"
+                                DrowBall.Text = Val(DrowBall.Text) + 1
+                            Else
+
+                            End If
+                        End If
+                    End If
+                End If
+            End If
+        End If
+        PictureBox4.Visible = True
+        Talk.Visible = True
+    End Sub
+    Public Sub obn()
+        ' ****************************
+        '     первый вброс кубиков
+        '*****************************
+        If xod = 1 Then
+            ' ****************************
+            '         левый кубик
+            '*****************************
+            If a = 1 Then gk1.Image = WindowsApplication1.My.Resources._1
+            If a = 2 Then gk1.Image = WindowsApplication1.My.Resources._2
+            If a = 3 Then gk1.Image = WindowsApplication1.My.Resources._3
+            If a = 4 Then gk1.Image = WindowsApplication1.My.Resources._4
+            If a = 5 Then gk1.Image = WindowsApplication1.My.Resources._5
+            If a = 6 Then gk1.Image = WindowsApplication1.My.Resources._6
+            ' ****************************
+            '        правый кубик
+            '*****************************
+            If b = 1 Then gk1_2.Image = WindowsApplication1.My.Resources._1_2
+            If b = 2 Then gk1_2.Image = WindowsApplication1.My.Resources._2_2
+            If b = 3 Then gk1_2.Image = WindowsApplication1.My.Resources._3_2
+            If b = 4 Then gk1_2.Image = WindowsApplication1.My.Resources._4_2
+            If b = 5 Then gk1_2.Image = WindowsApplication1.My.Resources._5_2
+            If b = 6 Then gk1_2.Image = WindowsApplication1.My.Resources._6_2
+        End If
+        ' ****************************
+        '     второй вброс кубиков
+        '*****************************
+        If xod = 2 Then
+            ' ****************************
+            '         левый кубик
+            '*****************************
+            If a = 1 Then gk2.Image = WindowsApplication1.My.Resources._1
+            If a = 2 Then gk2.Image = WindowsApplication1.My.Resources._2
+            If a = 3 Then gk2.Image = WindowsApplication1.My.Resources._3
+            If a = 4 Then gk2.Image = WindowsApplication1.My.Resources._4
+            If a = 5 Then gk2.Image = WindowsApplication1.My.Resources._5
+            If a = 6 Then gk2.Image = WindowsApplication1.My.Resources._6
+            If b = 1 Then gk2_2.Image = WindowsApplication1.My.Resources._1_2
+            If b = 2 Then gk2_2.Image = WindowsApplication1.My.Resources._2_2
+            If b = 3 Then gk2_2.Image = WindowsApplication1.My.Resources._3_2
+            If b = 4 Then gk2_2.Image = WindowsApplication1.My.Resources._4_2
+            If b = 5 Then gk2_2.Image = WindowsApplication1.My.Resources._5_2
+            If b = 6 Then gk2_2.Image = WindowsApplication1.My.Resources._6_2
+        End If
+        ' ****************************
+        '     третий вброс кубиков
+        '*****************************
+        If xod = 3 Then
+            ' ****************************
+            '         левый кубик
+            '*****************************
+            If a = 1 Then gk3.Image = WindowsApplication1.My.Resources._1
+            If a = 2 Then gk3.Image = WindowsApplication1.My.Resources._2
+            If a = 3 Then gk3.Image = WindowsApplication1.My.Resources._3
+            If a = 4 Then gk3.Image = WindowsApplication1.My.Resources._4
+            If a = 5 Then gk3.Image = WindowsApplication1.My.Resources._5
+            If a = 6 Then gk3.Image = WindowsApplication1.My.Resources._6
+            ' ****************************
+            '        правый кубик
+            '*****************************
+            If b = 1 Then gk3_2.Image = WindowsApplication1.My.Resources._1_2
+            If b = 2 Then gk3_2.Image = WindowsApplication1.My.Resources._2_2
+            If b = 3 Then gk3_2.Image = WindowsApplication1.My.Resources._3_2
+            If b = 4 Then gk3_2.Image = WindowsApplication1.My.Resources._4_2
+            If b = 5 Then gk3_2.Image = WindowsApplication1.My.Resources._5_2
+            If b = 6 Then gk3_2.Image = WindowsApplication1.My.Resources._6_2
+        End If
+
+    End Sub
+    ' ****************************
+    '     компьютер, формула
+    '*****************************
+    Public Sub II()
+        If (Val(MyBall.Text) <= 21 And Val(MyBall.Text) > Val(PCBall.Text)) Or xod = 1 Then
+            a = Int(Rnd() * 6) + 1
+            b = Int(Rnd() * 6) + 1
+            PCBall.Text = Val(PCBall.Text) + a + b
+            obn()  
+        End If
+    End Sub
+    Public Sub Clears()
+        gk1.Image = Nothing
+        gk1.Image = Nothing
+        gk1.Image = Nothing
+        gk1.Image = Nothing
+        gk1.Image = Nothing
+        gk1.Image = Nothing
+        gk1_2.Image = Nothing
+        gk1_2.Image = Nothing
+        gk1_2.Image = Nothing
+        gk1_2.Image = Nothing
+        gk1_2.Image = Nothing
+        gk1_2.Image = Nothing
+        gk2.Image = Nothing
+        gk2.Image = Nothing
+        gk2.Image = Nothing
+        gk2.Image = Nothing
+        gk2.Image = Nothing
+        gk2.Image = Nothing
+        gk2_2.Image = Nothing
+        gk2_2.Image = Nothing
+        gk2_2.Image = Nothing
+        gk2_2.Image = Nothing
+        gk2_2.Image = Nothing
+        gk2_2.Image = Nothing
+        gk3.Image = Nothing
+        gk3.Image = Nothing
+        gk3.Image = Nothing
+        gk3.Image = Nothing
+        gk3.Image = Nothing
+        gk3.Image = Nothing
+        gk3_2.Image = Nothing
+        gk3_2.Image = Nothing
+        gk3_2.Image = Nothing
+        gk3_2.Image = Nothing
+        gk3_2.Image = Nothing
+        gk3_2.Image = Nothing
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Randomize()
+        AllowTransparency = True
+        PictureBox1.BackColor = Color.Transparent
+        PictureBox2.BackColor = Color.Transparent
+        PictureBox3.BackColor = Color.Transparent
+        gk1.BackColor = Color.Transparent
+        gk1_2.BackColor = Color.Transparent
+        gk2.BackColor = Color.Transparent
+        gk2_2.BackColor = Color.Transparent
+        gk3.BackColor = Color.Transparent
+        gk3_2.BackColor = Color.Transparent
+        MyBall.BackColor = Color.Transparent
+        PCBall.BackColor = Color.Transparent
+        WinBall.BackColor = Color.Transparent
+        DrowBall.BackColor = Color.Transparent
+        LoseBall.BackColor = Color.Transparent
+        PictureBox4.BackColor = Color.Transparent
+    End Sub
+    ' ****************************
+    '       кнопка игра
+    '*****************************
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+        PictureBox4.Visible = False
+        Talk.Visible = False
+        PictureBox2.Enabled = True
+        PictureBox2.Visible = True
+        PictureBox3.Visible = True
+        PictureBox1.Left = 160
+        PictureBox1.Top = 305
+
+        Clears()
+        MyBall.Text = 0
+        PCBall.Text = 0
+        xod = 0
+    End Sub
+    ' ****************************
+    '         кнопка еще
+    '*****************************
+    Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click
+        PictureBox3.Enabled = True
+        xod = xod + 1
+        If xod < 4 Then
+1:          a = Int(Rnd() * 6) + 1
+            b = Int(Rnd() * 6) + 1
+            If a = 0 Or b = 0 Then GoTo 1
+            obn()
+            MyBall.Text = Val(MyBall.Text) + a + b
+        Else
+            MsgBox("Максимум 3 хода")
+        End If
+    End Sub
+    ' ****************************
+    '        кнопка стоп
+    '*****************************
+    Private Sub PictureBox3_Click(sender As Object, e As EventArgs) Handles PictureBox3.Click
+        Clears()
+        For xod = 1 To 3
+            II()
+        Next
+        Pob()
+        PictureBox2.Enabled = False
+        PictureBox3.Enabled = False
+        If LoseBall.Text >= 10 Then
+            Talk.Visible = True
+            Talk.Text = "Вы проиграли игру!"
+            LoseBall.Text = 0
+            DrowBall.Text = 0
+            WinBall.Text = 0
+        End If
+        If WinBall.Text >= 10 Then
+            Talk.Visible = True
+            Talk.Text = "Вы выиграли игру!"
+            LoseBall.Text = 0
+            DrowBall.Text = 0
+            WinBall.Text = 0
+        End If
+        If DrowBall.Text >= 10 Then
+            Talk.Visible = True
+            Talk.Text = "Игра закончилась ничьей!"
+            LoseBall.Text = 0
+            DrowBall.Text = 0
+            WinBall.Text = 0
+        End If
+    End Sub
+End Class
